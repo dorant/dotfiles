cea207e04eaea271f61cb6f7f43634a4578ab33d
Björn Svensson <bjorn.d.svensson@gmail.com>
2017-07-20 13:23:34 +0200

    Adding screen-handling and keymap

---
+++ i3/setup_screens.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+PRIMARY="eDP1"
+EXT1="DP1"
+EXT2="DVI-I-1-1"
+
+PRESENTATION="HDMI1"
+
+if (xrandr | grep "$EXT1 connected"); then
+    xrandr --output $PRESENTATION --off
+    xrandr --output $PRIMARY --primary --auto --output $EXT1 --auto --above $PRIMARY --output $EXT2 --auto --right-of $EXT1
+    echo "second screen enabled"
+elif (xrandr | grep "$PRESENTATION connected"); then
+    xrandr --output $EXT1 --off
+    xrandr --output $EXT2 --off 
+    xrandr --output $PRIMARY --primary --auto --output $PRESENTATION --auto --above $PRIMARY
+    echo "second screen (HDMI) enabled"
+else
+    xrandr --output $EXT1 --off
+    xrandr --output $EXT2 --off
+    xrandr --output $PRESENTATION --off
+    xrandr --output $PRIMARY --primary --auto
+    echo "second screen(s) disabled"
+fi
