cfbe165dc1ba00e8ce02e286ef1fc85b82cc1c20
Björn Svensson A <bjorn.d.svensson@gmail.com>
2018-06-17 15:31:46 +0200

    Improved presentation port (hdmi) handling

--- i3/setup_screens.sh
+++ i3/setup_screens.sh
@@ -6,22 +6,31 @@ EXT2="DVI-I-1-1"
 
 PRESENTATION="HDMI1"
 
-if (xrandr | grep "$EXT1 connected"); then
+# Arguments:
+#   toggle: toggle-once positions of screens, mirror or above
+PRESENTATION_POSITION="--same-as"
+EXT1_POSITION="--above"
+if [ "$1" == "toggle" ]; then
+    PRESENTATION_POSITION="--above"
+    EXT1_POSITION="--same-as"
+fi
+
+if (xrandr | grep "$PRESENTATION connected"); then
+    xrandr --output $EXT1 --off
+    xrandr --output $EXT2 --off
+    xrandr --output $PRIMARY --primary --auto --output $PRESENTATION $PRESENTATION_POSITION $PRIMARY
+    echo "second screen (HDMI) enabled"
+
+elif (xrandr | grep "$EXT1 connected"); then
     xrandr --output $PRESENTATION --off
 
     if (xrandr | grep "$EXT2 connected"); then
         xrandr --output $PRIMARY --primary --auto --output $EXT2 --auto --above $PRIMARY --output $EXT1 --auto --right-of $EXT2
         echo "second screens enabled ($EXT1,$EXT2)"
     else
-        xrandr --output $PRIMARY --primary --auto --output $EXT1 --auto --above $PRIMARY
+        xrandr --output $PRIMARY --primary --auto --output $EXT1 --auto $EXT1_POSITION $PRIMARY
         echo "second screen enabled ($EXT1)"
     fi
-
-elif (xrandr | grep "$PRESENTATION connected"); then
-    xrandr --output $EXT1 --off
-    xrandr --output $EXT2 --off
-    xrandr --output $PRIMARY --primary --auto --output $PRESENTATION --auto --above $PRIMARY
-    echo "second screen (HDMI) enabled"
 
 else
     xrandr --output $EXT1 --off
